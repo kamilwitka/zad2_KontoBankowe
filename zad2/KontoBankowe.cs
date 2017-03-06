adb4fc8bc8b326c2a7ee1509810cf798d55b8348
Kamil W <df>
2017-03-06 21:29:01 +0100

    Project

---
+++ zad2/KontoBankowe.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    class KontoBankowe
+    {
+        string imie;
+        string nazwisko;
+        int numerKonta;
+        int fundusze;
+        int id;
+
+        public KontoBankowe(string imie, string nazwisko, int numerKonta, int fundusze, int id)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.numerKonta = numerKonta;
+            this.fundusze = fundusze;
+            this.id = id;
+        }
+
+        public void wplacPieniadze(int fundusze)
+        {
+            Console.WriteLine("Na konto wpłacono " + fundusze + " zł");
+        }
+        public void zmienFunduszeKonta(int fundusze)
+        {
+            wplacPieniadze(fundusze);
+            Console.WriteLine("Podaj wartość odsetek: ");
+            double odsetki = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Zmieniono fundusze konta dzięki odsetkom. Po operacji fundusze to: " + fundusze * odsetki + " zł");
+        }
+        public bool usunKonto()
+        {
+            Console.WriteLine("Czy usunąć konto? y/n ");
+            string czyUsunac = Console.ReadLine();
+
+            if (czyUsunac == "y")
+            {
+                Console.WriteLine("Konto zostało usunięte");
+            }
+            return false;
+
+        }
+
+    }
+}
---
+++ zad2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KontoBankowe konto = new KontoBankowe("Kamil", "Witka", 123456, 0, 1);
+            
+            konto.zmienFunduszeKonta(1000);
+            konto.usunKonto();
+
+            Console.ReadLine();
+        }
+    }
+}
